493911a660017fc27708874d0413e795eda2a336
Kagiso-Obed-Makubjana <deepercreatives@gmail.com>
2026-03-05 09:12:32 +0200

    Add Azure provider and AKS resource configuration

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "cicd-rg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "cicd-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "cicdaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
